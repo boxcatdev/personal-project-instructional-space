94c83bae18cc972362a56283dafe563fa3c72c6e
Casey Rubio <127157790+gardenbud@users.noreply.github.com>
2023-04-04 18:22:51 -0500

    main menu scene manager script

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit the game!");
+    }
+}
